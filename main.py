dc4834b01ac6bdbca24dea13a6a4a833f1ea8eb6
linksintime <hellothereme12@gmail.com>
2022-06-18 23:33:11 -0400

    Introduced error checking in case you input something that doesn't exist; Reformatted

--- main.py
+++ main.py
@@ -11,22 +11,24 @@ def show_data():
     print("API data:\n")
     pprint(data)
 
+def errCheck(weather_data):
+    if weather_data['cod'] == '404':
+        raise KeyError("Couldn't locate city")
+
 API_KEY = 'bc50760d54528b10f34315f689817b3c'
 
-#city = input("Enter a city: ")
+city = input("Enter a city: ")
 # Grabbing weather data
-city = "Tokyo"
+#city = "Tokyo"
 
 base_url =  "http://api.openweathermap.org/data/2.5/weather?appid="+API_KEY+"&q="+city
 
 data = requests.get(base_url).json()
-
 #pprint(weather_data)
 # Grabbing the json dump from weather api
 dataDump = json.dumps(data)
 weather_data = json.loads(dataDump)
-
-# Print stuff
+errCheck(weather_data)
 
 # weather_descriptions = {"🌤",["🌦"],["⛆"],["⛈"],["☀"],["🌨"],["☁"],["☂"],["☄"],["⛇"],["⛈"],["🌥"],["🌩"],["🌧"],["🌩"]}
 weather_descriptions = {'clear sky': "☀", 'scattered clouds': "🌤 ☁ ☁", 'mist': "⛆⛆⛆", 'overcast clouds': "🌤", 'broken clouds': "☁ ☁ ☁"}
@@ -43,12 +45,10 @@ description = [i for i in weather_data['weather']]
 description = description[0]['description']
 # print(f"Weather feels like: {0:02d}".format(convert_kelvin_to_fahrenheit(weather_data['main']['feels_like']))
 print(f"Current weather in {city}\n")
-print(f"Weather is currently `{description}`: {weather_descriptions[description]}")
-print("Temperature currently feels like: {0:.1f}° fahrenheit\n".format(convert_kelvin_to_fahrenheit(feels_like)))
-
+print(f"Weather is currently `{description}`: {weather_descriptions[description]}\n")
+print("Temperature currently feels like: {0:.1f}° fahrenheit".format(convert_kelvin_to_fahrenheit(feels_like)))
 print("Today has an expected forcasted of {0:.1f}° high and a low of {1:.1f}°".format(max_temp, min_temp))
-
-print(f"Humidity: {humidity}%")
+print(f"Humidity: {humidity}%\n")
 
 print(f"Sunrise: {sunrise}")
 print(f"Sunset: {sunset}")
