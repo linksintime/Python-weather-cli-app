26716cd6ff9fcd95680e15c1b009844832cd2886
linksintime <hellothereme12@gmail.com>
2022-06-20 21:27:17 -0400

    Added comments so the thing was actually readable; Formatted to make it look prettier; Last but not least, has terminal colors >:)

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ import json
 from pprint import pprint
 import time
 
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+# Changes color based on temp (eg. temp > 100 = red, temp <= 80 = orange, temp <= 60 = blue)
+# NOTE: WIP
+def tempRangeSystem(temp):
+    pass
+
 def convert_kelvin_to_fahrenheit(value):
     return (value*1.8)-459.67
 
@@ -17,9 +34,9 @@ def errCheck(weather_data):
 
 API_KEY = 'bc50760d54528b10f34315f689817b3c'
 
-city = input("Enter a city: ")
+#city = input("Enter a city: ")
 # Grabbing weather data
-#city = "Tokyo"
+city = "Boston"
 
 base_url =  "http://api.openweathermap.org/data/2.5/weather?appid="+API_KEY+"&q="+city
 
@@ -33,22 +50,29 @@ errCheck(weather_data)
 # weather_descriptions = {"🌤",["🌦"],["⛆"],["⛈"],["☀"],["🌨"],["☁"],["☂"],["☄"],["⛇"],["⛈"],["🌥"],["🌩"],["🌧"],["🌩"]}
 weather_descriptions = {'clear sky': "☀", 'scattered clouds': "🌤 ☁ ☁", 'mist': "⛆⛆⛆", 'overcast clouds': "🌤", 'broken clouds': "☁ ☁ ☁", "light rain": "🌤 ⛆"}
 
+# Getting information from weather JSON File
 sunrise = time.ctime(weather_data['sys']['sunrise'])
 sunset = time.ctime(weather_data['sys']['sunset'])
 feels_like = weather_data['main']['feels_like']
 humidity = weather_data['main']['humidity']
 max_temp = convert_kelvin_to_fahrenheit(weather_data['main']['temp_max'])
 min_temp = convert_kelvin_to_fahrenheit(weather_data['main']['temp_min'])
-
 city = weather_data['name']
 description = [i for i in weather_data['weather']]
 description = description[0]['description']
-# print(f"Weather feels like: {0:02d}".format(convert_kelvin_to_fahrenheit(weather_data['main']['feels_like']))
-print(f"Current weather in {city}\n")
-print(f"Weather is currently `{description}`: {weather_descriptions[description]}\n")
-print("Temperature currently feels like: {0:.1f}° fahrenheit".format(convert_kelvin_to_fahrenheit(feels_like)))
-print("Today has an expected forcasted of {0:.1f}° high and a low of {1:.1f}°".format(max_temp, min_temp))
-print(f"Humidity: {humidity}%\n")
 
-print(f"Sunrise: {sunrise}")
-print(f"Sunset: {sunset}")
+### What user actually sees
+
+# Current weather description and city
+print(f"{color.BOLD}Current weather in: {color.END}{color.BLUE}{city}{color.END}\n")
+print(f"{color.BOLD}Weather is currently {color.END}{color.BLUE}`{description}`{color.END}{color.BOLD}: {weather_descriptions[description]}{color.END}\n")
+
+# Current Temperature, Max Temperature, Min Temperature with nice formatting and colors! >:)
+print(f"{color.BOLD}Temperature currently feels like: {color.END}{color.BLUE}{convert_kelvin_to_fahrenheit(feels_like):.1f}{color.END}°{color.BOLD}fahrenheit{color.END}")
+print(f"{color.BOLD}Today has an expected forcasted of {color.END}{color.BLUE}{max_temp:.1f}°{color.END}{color.BOLD} high and a low of {color.END}{color.BLUE}{min_temp:.1f}°{color.END}\n")
+
+# Humidity
+print(f"{color.BOLD}Humidity:{color.END} {color.BLUE}{humidity}%{color.END}\n")
+
+print(f"{color.BOLD}Sunrise: {color.END}{color.BLUE}{sunrise}{color.END}")
+print(f"{color.BOLD}Sunset: {color.END}{color.BLUE}{sunset}{color.END}")
