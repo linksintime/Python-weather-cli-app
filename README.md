1eb0dec5f077e54f866aa0e48624b886f646a230
linksintime <hellothereme12@gmail.com>
2022-06-20 20:52:52 -0400

    Edited README.md and created debugging tools for main.py

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Weather API
+# Weather API Debug
 
 Creating a weather API to help me practice and learn about what an API is in general.
 
@@ -14,3 +14,18 @@ I believe thats going to be it for now...
 >   * Regarded as the 3 cornerstones of Web Development
 ***
 > * *Mainly* try not to die inside when I can't do it...
+
+<br>
+
+# **Edit**
+
+Branch was created for debugging the file.
+
+> **Program takes a list of cities**
+> * Iterates through the list
+> * Mission is to find weather descriptions that aren't in the code so that it doesn't throw an error.
+
+*Thats all I have now*
+
+***-Linksintime***
+
--- main.py
+++ main.py
@@ -17,38 +17,54 @@ def errCheck(weather_data):
 
 API_KEY = 'bc50760d54528b10f34315f689817b3c'
 
-city = input("Enter a city: ")
+#city = input("Enter a city: ")
 # Grabbing weather data
 #city = "Tokyo"
+oldList = []
+timeStart = time.time()
+with open("cities.txt", "r", encoding="utf-8") as oregonCities:
+    cities = [i for i in oregonCities.readlines()]
 
-base_url =  "http://api.openweathermap.org/data/2.5/weather?appid="+API_KEY+"&q="+city
+cities = sorted(cities)
+errCities = []
 
-data = requests.get(base_url).json()
-#pprint(weather_data)
-# Grabbing the json dump from weather api
-dataDump = json.dumps(data)
-weather_data = json.loads(dataDump)
-errCheck(weather_data)
+for j in cities:
+    
+    base_url =  "http://api.openweathermap.org/data/2.5/weather?appid="+API_KEY+"&q="+j
+    data = requests.get(base_url).json()
+    #pprint(weather_data)
+    # Grabbing the json dump from weather api
+    dataDump = json.dumps(data)
+    weather_data = json.loads(dataDump)
+    #errCheck(weather_data)
+    # weather_descriptions = {"🌤",["🌦"],["⛆"],["⛈"],["☀"],["🌨"],["☁"],["☂"],["☄"],["⛇"],["⛈"],["🌥"],["🌩"],["🌧"],["🌩"]}
+    weather_descriptions = {'clear sky': "☀", 'scattered clouds': "🌤 ☁ ☁", 'mist': "⛆⛆⛆", 'overcast clouds': "🌤", 'broken clouds': "☁ ☁ ☁", "light rain": "🌤 ⛆", "few clouds": "☀  ☁", "moderate rain": '🌦 🌧', "light intensity drizzle":"⛆", "heavy intensity rain":"🌧🌧🌧🌧🌧"}
+    #sunrise = time.ctime(weather_data['sys']['sunrise'])
+    #sunset = time.ctime(weather_data['sys']['sunset'])
+    try:
+        feels_like = weather_data['main']['feels_like']
+        city = weather_data['name']
+        description = [i for i in weather_data['weather']]
+        description = description[0]['description']
+    except:
+        #print(f"Couldn't find city {j}")
+        errCities.append(j)
+    #humidity = weather_data['main']['humidity']
+    #max_temp = convert_kelvin_to_fahrenheit(weather_data['main']['temp_max'])
+    #min_temp = convert_kelvin_to_fahrenheit(weather_data['main']['temp_min'])
+    
+    # print(f"Weather feels like: {0:02d}".format(convert_kelvin_to_fahrenheit(weather_data['main']['feels_like']))
+    #print(f"Current weather in {city}")
+    #print(f"Weather is currently `{description}`: {weather_descriptions[description]}")
+    #print("Temperature currently feels like: {0:.1f}° fahrenheit".format(convert_kelvin_to_fahrenheit(feels_like)))
+    #print("Today has an expected forcasted of {0:.1f}° high and a low of {1:.1f}°".format(max_temp, min_temp))
+    #print(f"Humidity: {humidity}%\n")
+    #print(f"Sunrise: {sunrise}")
+    #print(f"Sunset: {sunset}")
+with open("errorCities.txt", "w", encoding="utf-8") as badCity:
+    for i in errCities: badCity.writelines(i)
 
-# weather_descriptions = {"🌤",["🌦"],["⛆"],["⛈"],["☀"],["🌨"],["☁"],["☂"],["☄"],["⛇"],["⛈"],["🌥"],["🌩"],["🌧"],["🌩"]}
-weather_descriptions = {'clear sky': "☀", 'scattered clouds': "🌤 ☁ ☁", 'mist': "⛆⛆⛆", 'overcast clouds': "🌤", 'broken clouds': "☁ ☁ ☁", "light rain": "🌤 ⛆"}
-
-sunrise = time.ctime(weather_data['sys']['sunrise'])
-sunset = time.ctime(weather_data['sys']['sunset'])
-feels_like = weather_data['main']['feels_like']
-humidity = weather_data['main']['humidity']
-max_temp = convert_kelvin_to_fahrenheit(weather_data['main']['temp_max'])
-min_temp = convert_kelvin_to_fahrenheit(weather_data['main']['temp_min'])
-
-city = weather_data['name']
-description = [i for i in weather_data['weather']]
-description = description[0]['description']
-# print(f"Weather feels like: {0:02d}".format(convert_kelvin_to_fahrenheit(weather_data['main']['feels_like']))
-print(f"Current weather in {city}\n")
-print(f"Weather is currently `{description}`: {weather_descriptions[description]}\n")
-print("Temperature currently feels like: {0:.1f}° fahrenheit".format(convert_kelvin_to_fahrenheit(feels_like)))
-print("Today has an expected forcasted of {0:.1f}° high and a low of {1:.1f}°".format(max_temp, min_temp))
-print(f"Humidity: {humidity}%\n")
-
-print(f"Sunrise: {sunrise}")
-print(f"Sunset: {sunset}")
+timeStop = time.time
+totalTime = timeStop - timeStart
+print(errCities)
+print(totalTime)
